8bf2b28ca0995431ad9d116c7e139e7978304d29
alvcarber1 <acarrerabernal@gmail.com>
2024-04-25 20:17:00 +0200

    Arreglos

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ async function conectarMongoDB() {
 }
 
 async function obtenerTiposDeColeccion() {
-<<<<<<< HEAD
   try {
      const db = await conectarMongoDB();
      const colecciones = await db.listCollections().toArray();
@@ -120,49 +119,9 @@ app.get('/producto/:id', async (req, res) => {
     if (!ObjectId.isValid(productoId)) {
       return res.status(400).send('ID de producto no válido');
     }
-=======
- try {
-    const db = await conectarMongoDB();
-    const colecciones = await db.listCollections().toArray();
-    return colecciones.map(c => c.name);
- } catch (error) {
-    console.error('Error al obtener tipos de colección:', error);
-    throw error;
- }
-}
-
-async function agregarProducto(tipo, producto) {
- try {
-    const db = await conectarMongoDB();
-    const result = await db.collection(tipo).insertOne(producto);
-    console.log('Producto agregado:', result);
-    return result;
- } catch (error) {
-    console.error('Error al agregar producto:', error);
-    throw error;
- }
-}
-
-// Ruta para mostrar el formulario de añadir producto
-app.get('/productos/agregar', async (req, res) => {
- try {
-    let tiposDeColeccion = await obtenerTiposDeColeccion();
-    res.render('layout', { content: 'add-product-form', tiposDeColeccion });
- } catch (error) {
-    console.error('Error al mostrar formulario de agregar producto:', error);
-    res.status(500).send('Error interno del servidor');
- }
-});
-
-// Ruta para manejar el formulario de añadir producto
-app.post('/productos/agregar', async (req, res) => {
- try {
-    const { tipo, nombre, descripcion, precio, tallas, colores, cantidad, imagenes, 'proveedor-nombre': proveedorNombre, 'proveedor-contacto': proveedorContacto } = req.body;
->>>>>>> 5e0b325f72ae14308e01aafcaccea0c072bcc49e
 
     console.log('Buscando producto con ID:', productoId); // Registro de depuración
 
-<<<<<<< HEAD
     let producto = await db.collection('camisetas').findOne({ _id: new ObjectId(productoId) });
 
     if (!producto) {
@@ -196,68 +155,6 @@ app.post('/productos/agregar', async (req, res) => {
     if (producto) {
       res.render('layout', { content: 'product-details', producto: producto });
     } else {
-=======
-    await agregarProducto(tipo, producto);
-    res.redirect('/productos');
- } catch (error) {
-    console.error('Error al procesar formulario de agregar producto:', error);
-    res.status(500).send('Error interno del servidor');
- }
-});
-
-// Ruta para mostrar el listado de productos
-app.get('/productos', async (req, res) => {
- try {
-    let tiposDeColeccion = await obtenerTiposDeColeccion();
-    let tipoSeleccionado = req.query.tipo || "camisetas"; // Si no se especifica tipo, se usa "camisetas"
-    const db = client.db(dbName);
-
-    if (tiposDeColeccion.includes(tipoSeleccionado)) {
-      productos = await db.collection(tipoSeleccionado).find({}).toArray();
-      res.render('layout', { content: 'search-form', tiposDeColeccion });
-    } else {
-      res.status(404).send('Tipo de producto no válido');
-    }
- } catch (error) {
-    console.error('Error al obtener tipos de colección:', error);
-    res.status(500).send('Error interno del servidor');
- }
-});
-
-// Ruta para mostrar los detalles de un producto
-app.get('/producto/:id', async (req, res) => {
-  try {
-    const db = await conectarMongoDB();
-
-    const productoId = req.params.id;
-
-    // Comprobar si productoId es un ObjectId válido
-    if (!ObjectId.isValid(productoId)) {
-      return res.status(400).send('ID de producto no válido');
-    }
-
-    console.log('Buscando producto con ID:', productoId); // Registro de depuración
-
-    let producto = await db.collection('camisetas').findOne({ _id: new ObjectId(productoId) });
-
-    if (!producto) {
-      producto = await db.collection('pantalones').findOne({ _id: new ObjectId(productoId) });
-    }
-
-    if (!producto) {
-      producto = await db.collection('faldas').findOne({ _id: new ObjectId(productoId) });
-    }
-
-    if (!producto) {
-      producto = await db.collection('sudaderas').findOne({ _id: new ObjectId(productoId) });
-    }
-
-    console.log('Producto encontrado:', producto); // Registro de depuración
-
-    if (producto) {
-      res.render('layout', { content: 'product-details', producto: producto });
-    } else {
->>>>>>> 5e0b325f72ae14308e01aafcaccea0c072bcc49e
       res.status(404).send('Producto no encontrado en ninguna colección');
     }
   } catch (error) {
@@ -265,22 +162,6 @@ app.get('/producto/:id', async (req, res) => {
     res.status(500).send('Error interno del servidor');
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
- 
 
 // Define una ruta para la raíz de la aplicación
 app.get('/', (req, res) => {
